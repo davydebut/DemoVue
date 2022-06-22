c80c8bdb44896c27b40ddb69ba54fff893fae026
davydebut <spanudavy@gmail.com>
2022-06-22 16:46:04 +0200

    commentaires ajoute

--- Stagiaire.js
+++ Stagiaire.js
@@ -1,8 +1,9 @@
 export default {
+    //"les données descendent", ici, le parent envoie le prénom sous la forme d'un props
     props: {
         prenom: String
     },
-
+    //"l'état remonte", par exemple ici l'enfant ne peut pas supprimer dans le tableau du parent, on emet donc un évenement (qu'il va falloir écouter au niveau du parent)
     methods: {
         askToSuppress() {
             this.$emit('asktosuppress', this.prenom)
